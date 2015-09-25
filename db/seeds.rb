86e0ea640280c3126103a214a55e003e5a49eb35
Hammad Khalid <hammad.khalid@shopify.com>
2015-09-24 23:07:08 -0400

    Add a seeds file for locations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+location_list = [
+  [ "Canada", 2015],
+]
+
+location_list.each do |name, year|
+  Location.where( name: name, year: year ).first_or_create!
+end
