9c5de0f45df5ed210f31ca53832f2cbecb23f277
Hammad Khalid <hammad.khalid@shopify.com>
2015-09-26 15:37:57 -0400

    Update tests for elections controller

--- test/controllers/elections_controller_test.rb
+++ test/controllers/elections_controller_test.rb
@@ -5,13 +5,19 @@ class ElectionsControllerTest < ActionController::TestCase
     @election ||= elections(:canada)
   end
 
-  test "should get index" do
+  test "#index is successful" do
     get :index
     assert_response :success
   end
 
-  test "should get show" do
+  test "#show is successful" do
     get :show, id: election.slug
     assert_response :success
+  end
+
+  test "#show is not successful when election does not exist" do
+    assert_raise ActionView::Template::Error do
+      get :show, id: "does not exist"
+    end
   end
 end
