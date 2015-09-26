e3072d743336a710f60e098474208f706d37c866
Hammad Khalid <hammad.khalid@shopify.com>
2015-09-26 15:17:39 -0400

    Add a slug to the model

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,8 +1,17 @@
 class Election < ActiveRecord::Base
   validates :name, presence: true
   validates :year, presence: true
+  validates :slug, presence: true, uniqueness: true
+
+  before_validation :generate_slug
 
   def to_param
-    name
+    slug
+  end
+
+  private
+
+  def generate_slug
+    self.slug ||= "#{name}-#{year}".parameterize
   end
 end
--- test/models/election_test.rb
+++ test/models/election_test.rb
@@ -16,7 +16,18 @@ class ElectionTest < ActiveSupport::TestCase
     refute election.valid?
   end
 
-  test "#to_param returns the election's name" do
-    assert_equal election.name, election.to_param
+  test "Election slug must be unique" do
+    new_election = Election.new(name: "New country", year: 2015, slug: election.slug)
+    refute new_election.valid?
+  end
+
+  test "Election's, when not specified, is set to the name and year of the election" do
+    new_election = Election.new(name: "New country", year: 2015)
+    new_election.valid?
+    assert_equal new_election.slug, "#{new_election.name}-#{new_election.year}".parameterize
+  end
+
+  test "#to_param returns the election's slug" do
+    assert_equal election.slug, election.to_param
   end
 end
