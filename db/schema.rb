634e391dd06a93d435233c79e7a7d03f78675fc2
Hammad Khalid <hammad.khalid@shopify.com>
2015-09-27 21:27:02 -0400

    Add the answer model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150928004208) do
+ActiveRecord::Schema.define(version: 20150928012444) do
+
+  create_table "answers", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "candidate_id"
+    t.integer  "question_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
   create_table "candidates", force: :cascade do |t|
     t.string   "name"
